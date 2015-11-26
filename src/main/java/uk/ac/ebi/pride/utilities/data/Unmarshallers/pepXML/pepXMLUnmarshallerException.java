a129da0ca2001badc45452a187741e18ca8a7b11
SangZhe <zhe.sang@gmail.com>
2015-11-26 20:30:54 +0800

    add pepXML

---
+++ src/main/java/uk/ac/ebi/pride/utilities/data/Unmarshallers/pepXML/pepXMLUnmarshallerException.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.pride.utilities.data.Unmarshallers.pepXML;
+
+/**
+ * @author Florian Reisinger
+ * @since 0.4
+ */
+public class pepXMLUnmarshallerException extends Exception {
+
+    public pepXMLUnmarshallerException(String msg) {
+        super(msg);
+    }
+
+    public pepXMLUnmarshallerException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
